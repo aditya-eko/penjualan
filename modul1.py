67989f3dd0b36c20148f74dd13c10de0202b82f5
aditya-eko <ekoagusaditya@gmail.com>
2019-09-17 14:48:42 +0700

    menambahkan file modul dan gitignore

---
+++ modul1.py
@@ -0,0 +1,20 @@
+def menu():
+    print("restoran ini menyediakan menu sebagai berikut:")
+
+
+def gadoGado(x):
+    harga=10000
+    total=x*harga
+    print("harga yang harus anda bayar adalah",total)
+    return total
+
+
+def rujak(x):
+    harga=5000
+    total=x*harga
+    print("harga yang harus di bayar adalah", total)
+    return total
+
+
+gadoGado(1)
+rujak(3)
